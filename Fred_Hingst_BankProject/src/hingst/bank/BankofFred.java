6cfe2474c7f1731ff6c41e2921074d5f4d0d4b3e
Frederick Hingst <93553114+fhingst3@users.noreply.github.com>
2021-11-11 16:42:47 -0800

    Add files via upload

---
+++ Fred_Hingst_BankProject/src/hingst/bank/BankofFred.java
@@ -0,0 +1,12 @@
+package hingst.bank;
+
+//Set up input methods and screen, connectivity, etc.
+public class BankofFred {
+
+    private String firstName;
+    private String lastName;
+    private Double acctBalance;
+    private String acctType;
+    private Long acctNumber;
+
+}
---
+++ Fred_Hingst_BankProject/src/hingst/bank/util/ConnectionMaker.java
@@ -0,0 +1,7 @@
+package hingst.bank.util;
+
+//Import all the right stuff
+public class ConnectionMaker {
+
+    private static final ConnectionMaker connectionFactory = new ConnectionMaker();
+}
